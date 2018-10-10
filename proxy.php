2cdc68535d2f2fadd4777a300ece6915187a9614
Roderic Page <rdmpage@gmail.com>
2018-10-10 14:24:27 +0100

    Clean up

--- proxy.php
+++ proxy.php
@@ -5,7 +5,6 @@
 
 
 $url = $_GET['url'];
-$url = $_GET['url'];
 
 $postdata = '';
 if (isset($_GET['postdata']))
@@ -20,28 +19,27 @@ if (isset($_GET['callback']))
 }
 
 
-	$ch = curl_init(); 
-	curl_setopt ($ch, CURLOPT_URL, $url); 
-	curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1); 
-	curl_setopt ($ch, CURLOPT_COOKIEJAR, 'cookie.txt');	
-	curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, false);
-	
-	curl_setopt ($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json')); 
-	
+$ch = curl_init(); 
+curl_setopt ($ch, CURLOPT_URL, $url); 
+curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt ($ch, CURLOPT_COOKIEJAR, 'cookie.txt');	
+curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+curl_setopt ($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json')); 
+
+if ($postdata != '')
+{
 	curl_setopt($ch, CURLOPT_POST, TRUE);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
-	
-	$response = curl_exec($ch);
-	$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-	
-	if (curl_errno ($ch) != 0 )
-	{
-		echo "CURL error: ", curl_errno ($ch), " ", curl_error($ch);
-	}
-	
-	
-	//echo $http_code . "\n";
+}
 
+$response = curl_exec($ch);
+$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+if (curl_errno ($ch) != 0 )
+{
+	echo "CURL error: ", curl_errno ($ch), " ", curl_error($ch);
+}
 
 
 if ($callback != '')
